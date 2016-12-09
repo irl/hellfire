be191813d5cbc35a7e6f32ae6b4c9ccc27cbed99
Iain R. Learmonth <irl@fsfe.org>
2016-12-09 13:52:23 +0000

    Split into two wait groups

--- hellfire.go
+++ hellfire.go
@@ -3,9 +3,10 @@ package main
 import (
 	"flag"
 	"fmt"
-	"inputs"
+	"pathspider.net/hellfire/inputs"
 	"net"
 	"os"
+	"time"
 	"sync"
 )
 
@@ -17,7 +18,7 @@ func main() {
 
 	var testList inputs.TestList
 
-	if *topsites && (*citizenlab == "none") {
+	if *topsites && (*citizenlab != "none") {
 		panic("You can only select one source")
 	} else if !*topsites && (*citizenlab == "none") {
 		flag.PrintDefaults()
@@ -34,62 +35,80 @@ func main() {
 	performLookups(testList)
 }
 
-func lookupWorker(id int, waitGroup *sync.WaitGroup,
+func lookupWorker(id int, lookupWaitGroup *sync.WaitGroup,
 	jobs chan map[string]interface{},
 	results chan map[string]interface{}) {
-	waitGroup.Add(1)
+	lookupWaitGroup.Add(1)
 	fmt.Println("Spawning worker ", id)
-	go func(id int, jobs chan map[string]interface{}, results chan map[string]interface{}) {
-		defer waitGroup.Done()
+	go func(id int, lookupWaitGroup *sync.WaitGroup, jobs chan map[string]interface{}, results chan map[string]interface{}) {
+		defer lookupWaitGroup.Done()
 		for job := range jobs {
-			if job["domain"] == "" {
-				fmt.Println("Worker", id, "cascading shutdown signal")
+			if job["domain"] == nil {
 				jobs <- make(map[string]interface{})
+				fmt.Println("Worker", id, "cascaded shutdown signal")
 				break
 			}
 			fmt.Println("Worker", id, "got a job: ", job)
-			ips, _ := net.LookupIP(job["domain"].(string))
+			var attempt int
+			var ips []net.IP
+			for attempt = 0 ; attempt <= 3 ; attempt++ {
+				ips, _ = net.LookupIP(job["domain"].(string))
+				if len(ips) == 0 {
+					time.Sleep(1)
+				} else {
+					break
+				}
+			}
 			job["ips"] = ips
+			job["attempts"] = attempt
 			results <- job
 		}
-	}(id, jobs, results)
+		fmt.Println("Worker", id, "exiting")
+	}(id, lookupWaitGroup, jobs, results)
 }
 
-func outputPrinter(waitGroup *sync.WaitGroup, results chan map[string]interface{}) {
-	defer waitGroup.Done()
-	for {
-		result := <-results
-		if result["domain"] == nil {
-			break
+func outputPrinter(outputWaitGroup *sync.WaitGroup, results chan map[string]interface{}) {
+	outputWaitGroup.Add(1)
+	fmt.Println("Spawning output printer")
+	go func(results chan map[string]interface{}) {
+		defer outputWaitGroup.Done()
+		for {
+			result := <-results
+			if result["domain"] == nil {
+				fmt.Println("Output printer got shutdown signal")
+				break
+			}
+			fmt.Println("Result: ", result["domain"], result["ips"], result["attempts"])
 		}
-		fmt.Println("Result: ", result["domain"], result["ips"])
-	}
+	}(results)
 }
 
 func performLookups(testList inputs.TestList) {
-	var waitGroup sync.WaitGroup
-	var jobs chan map[string]interface{} = make(chan map[string]interface{})
+	var lookupWaitGroup sync.WaitGroup
+	var outputWaitGroup sync.WaitGroup
+	var jobs chan map[string]interface{} = make(chan map[string]interface{}, 1)
 	var results chan map[string]interface{} = make(chan map[string]interface{})
 
 	// Spawn lookup workers
 	for i := 0; i < 300; i++ {
-		lookupWorker(i, &waitGroup, jobs, results)
+		lookupWorker(i, &lookupWaitGroup, jobs, results)
 	}
 
 	// Spawn output printer
-	// The output printer doesn't lock the wait group until after the
-	// workers are done - maybe we should have two
-	go outputPrinter(&waitGroup, results)
+	outputPrinter(&outputWaitGroup, results)
 
 	// Submit jobs
 	testList.FeedJobs(jobs)
-	waitGroup.Wait()
+	jobs <- make(map[string]interface{})
+	lookupWaitGroup.Wait()
+	<- jobs // Read last shutdown sentinel from the queue left by the
+		// final worker to exit
+		// https://blog.golang.org/pipelines - This is a better way
+	fmt.Println("Workers now all finished")
 	close(jobs)
 
 	// Shutdown the output printer
-	waitGroup.Add(1)
-	shutdownSentinel := make(map[string]interface{})
-	results <- shutdownSentinel
+	results <- make(map[string]interface{})
+	outputWaitGroup.Wait()
 	close(results)
-	waitGroup.Wait()
 }
